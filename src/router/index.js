f3d138c40fdfc711b427a3d624370f77a32ec8e0
刘珅 <liushen@habby.com>
2025-05-20 01:03:18 +0800

    测试下

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import routes from './routes.js'
 
 let routeList = [];
@@ -11,7 +11,7 @@ for (let i = 0; i < routes.length; i++) {
 }
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: routeList
 })
 
