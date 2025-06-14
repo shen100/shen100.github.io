d02a9ad1f23f03151c6db4f054ef8fdbddba234a
刘珅 <liushen@habby.com>
2025-06-14 19:24:07 +0800

    update

--- src/trade/views/dashboard.vue
+++ src/trade/views/dashboard.vue
@@ -2,33 +2,31 @@
     <div>
 		<Card>
 			<div class="total-shizhi-txt">
-				<template v-if="data.shiZhi && data.shiZhi.amount">
-					<div>总市值: {{ (data.shiZhi.amount / 10000).toFixed(2) }}万亿 &nbsp;({{ data.shiZhi.count }}家) </div>
-					<Icon class="refresh" @click="requestAllStockDetail" type="md-refresh" style="cursor: pointer;" />
-					<div class="updated-at">{{ data.updatedAt1 ? '更新于 ' + data.updatedAt1 : '' }}</div>
-				</template>
+				<div v-if="data.shiZhi && data.shiZhi.amount">总市值: {{ (data.shiZhi.amount / 10000).toFixed(2) }}万亿 &nbsp;({{ data.shiZhi.count }}家) </div>
+				<Icon class="refresh" @click="requestAllStockDetail" type="md-refresh" style="cursor: pointer;" />
+				<div class="updated-at">{{ data.updatedAt1 ? '更新于 ' + data.updatedAt1 : '' }}</div>
 			</div>
 			<Table border :columns="data.columns" :data="data.shiZhiList">
 				<template #shiZhi0="{ row }">
 					<div>{{ row.shiZhi0.count }}家 ({{ row.shiZhi0.percent }}%)</div>
 				</template>
-				<template #shiZhi100="{ row }">
-					<div>{{ row.shiZhi100.count }}家 ({{ row.shiZhi100.percent }}%)</div>
+				<template #shiZhi1="{ row }">
+					<div>{{ row.shiZhi1.count }}家 ({{ row.shiZhi1.percent }}%)</div>
 				</template>
-				<template #shiZhi500="{ row }">
-					<div>{{ row.shiZhi500.count }}家 ({{ row.shiZhi500.percent }}%)</div>
+				<template #shiZhi2="{ row }">
+					<div>{{ row.shiZhi2.count }}家 ({{ row.shiZhi2.percent }}%)</div>
 				</template>
-				<template #shiZhi1000="{ row }">
-					<div>{{ row.shiZhi1000.count }}家 ({{ row.shiZhi1000.percent }}%)</div>
+				<template #shiZhi3="{ row }">
+					<div>{{ row.shiZhi3.count }}家 ({{ row.shiZhi3.percent }}%)</div>
 				</template>
-				<template #shiZhi2000="{ row }">
-					<div>{{ row.shiZhi2000.count }}家 ({{ row.shiZhi2000.percent }}%)</div>
+				<template #shiZhi4="{ row }">
+					<div>{{ row.shiZhi4.count }}家 ({{ row.shiZhi4.percent }}%)</div>
 				</template>
-				<template #shiZhi5000="{ row }">
-					<div>{{ row.shiZhi5000.count }}家 ({{ row.shiZhi5000.percent }}%)</div>
+				<template #shiZhi5="{ row }">
+					<div>{{ row.shiZhi5.count }}家 ({{ row.shiZhi5.percent }}%)</div>
 				</template>
-				<template #shiZhi10000="{ row }">
-					<div>{{ row.shiZhi10000.count }}家 ({{ row.shiZhi10000.percent }}%)</div>
+				<template #shiZhi6="{ row }">
+					<div>{{ row.shiZhi6.count }}家 ({{ row.shiZhi6.percent }}%)</div>
 				</template>
 			</Table>
 		</Card>
@@ -66,54 +64,54 @@ let data = ref({
 		},
 		{
 			title: '[100亿, 500亿)',
-			slot: 'shiZhi100'
+			slot: 'shiZhi1'
 		},
 		{
 			title: '[500亿, 1000亿)',
-			slot: 'shiZhi500'
+			slot: 'shiZhi2'
 		},
 		{
 			title: '[1000亿, 2000亿)',
-			slot: 'shiZhi1000'
+			slot: 'shiZhi3'
 		},
 		{
 			title: '[2000亿, 5000亿)',
-			slot: 'shiZhi2000'
+			slot: 'shiZhi4'
 		},
 		{
 			title: '[5000亿, 1万亿)',
-			slot: 'shiZhi5000'
+			slot: 'shiZhi5'
 		},
 		{
 			title: '1万亿以上',
-			slot: 'shiZhi10000'
+			slot: 'shiZhi6'
 		}
 	],
 	shiZhi0: { // 100亿以下
 		count: 0,
 		percent: 0,
 	},
-	shiZhi100: { // 100亿-500亿
+	shiZhi1: { // 100亿-500亿
 		count: 0,
 		percent: 0,
 	},
-	shiZhi500: { // 500亿-1000亿
+	shiZhi2: { // 500亿-1000亿
 		count: 0,
 		percent: 0,
 	},
-	shiZhi1000: { // 1000亿-2000亿
+	shiZhi3: { // 1000亿-2000亿
 		count: 0,
 		percent: 0,
 	},
-	shiZhi2000: { // 2000亿-5000亿
+	shiZhi4: { // 2000亿-5000亿
 		count: 0,
 		percent: 0,
 	},
-	shiZhi5000: { // 5000亿-1万亿
+	shiZhi5: { // 5000亿-1万亿
 		count: 0,
 		percent: 0,
 	},
-	shiZhi10000: { // 1万亿以上
+	shiZhi6: { // 1万亿以上
 		count: 0,
 		percent: 0,
 	},
@@ -128,7 +126,7 @@ let data = ref({
 });
 
 let legendData = [
-	'100亿以下', '[100, 500)', '[500, 1000)', '[1000, 2000)', '[2000, 5000)', '[5000, 1万亿)', '1万亿以上'
+	'小于100亿', '[100亿, 500亿)', '[500亿, 1000亿)', '[1000亿, 2000亿)', '[2000亿, 5000亿)', '[5000亿, 1万亿)', '1万亿以上', '全部'
 ];
 
 const chartOptions = ref({
@@ -187,12 +185,13 @@ function updateChart() {
 	data.value.updatedAt2 = store.compositeIndex.updatedAt;
 	let indexArr = [
 		'index0',
-		'index100',
-		'index500',
-		'index1000',		
-		'index2000',
-		'index5000',
-		'index10000'
+		'index1',
+		'index2',
+		'index3',
+		'index4',
+		'index5',
+		'index6',
+		'index'
 	];
 	let series = [];
 	let zhiShuSeries = [];
@@ -212,12 +211,12 @@ function updateChart() {
 		series.push({
 			name: legendData[i],
 			type: 'line',
-			data: arr.map(item => item.amount),
+			data: arr.map(item => Number(item.amount / 10000).toFixed(2)), // 转换为万亿
 		});
 		zhiShuSeries.push({
 			name: legendData[i],
 			type: 'line',
-			data: arr.map(item => item.amount / item.count),
+			data: arr.map(item => parseInt(item.amount / item.count)),
 		});
 
 		if (!allDates) {
@@ -239,12 +238,12 @@ function resetData() {
 	const defaultData = { count: 0, percent: 0 };
 	data.value.shiZhiList = [];
 	data.value.shiZhi0 = { ...defaultData };
-	data.value.shiZhi100 = { ...defaultData };
-	data.value.shiZhi500 = { ...defaultData };
-	data.value.shiZhi1000 = { ...defaultData };
-	data.value.shiZhi2000 = { ...defaultData };
-	data.value.shiZhi5000 = { ...defaultData };
-	data.value.shiZhi10000 = { ...defaultData };
+	data.value.shiZhi1 = { ...defaultData };
+	data.value.shiZhi2 = { ...defaultData };
+	data.value.shiZhi3 = { ...defaultData };
+	data.value.shiZhi4 = { ...defaultData };
+	data.value.shiZhi5 = { ...defaultData };
+	data.value.shiZhi6 = { ...defaultData };
 	data.value.shiZhi = {
 		amount: 0,
 		count: 0,
@@ -257,7 +256,7 @@ async function requestAllStockDetail() {
 	let concurrence = 100;
 	for (let i = 0; i < allStocks.length; i += concurrence) {
 		let tasks = [];
-		console.log('i', i, allStocks[i]);
+		console.log('requestAllStockDetail', i, new Date().toISOString());
 		for (let j = i; j < i + concurrence && j < allStocks.length; j++) {
 			tasks.push(requestStockDetail(allStocks[j]));
 		}
@@ -269,38 +268,38 @@ async function requestAllStockDetail() {
 				if (stock.zongShiZhi < 100) {
 					data.value.shiZhi0.count += 1;
 				} else if (stock.zongShiZhi < 500) {
-					data.value.shiZhi100.count += 1;
+					data.value.shiZhi1.count += 1;
 				} else if (stock.zongShiZhi < 1000) {
-					data.value.shiZhi500.count += 1;
+					data.value.shiZhi2.count += 1;
 				} else if (stock.zongShiZhi < 2000) {
-					data.value.shiZhi1000.count += 1;
+					data.value.shiZhi3.count += 1;
 				} else if (stock.zongShiZhi < 5000) {
-					data.value.shiZhi2000.count += 1;
+					data.value.shiZhi4.count += 1;
 				} else if (stock.zongShiZhi < 10000) {
-					data.value.shiZhi5000.count += 1;
+					data.value.shiZhi5.count += 1;
 				} else {
-					data.value.shiZhi10000.count += 1;
+					data.value.shiZhi6.count += 1;
 				}
 			}
 		}
 		let theCount = data.value.shiZhi.count;
 		data.value.shiZhi0.percent = (data.value.shiZhi0.count / theCount * 100).toFixed(2);
-		data.value.shiZhi100.percent = (data.value.shiZhi100.count / theCount * 100).toFixed(2);
-		data.value.shiZhi500.percent = (data.value.shiZhi500.count / theCount * 100).toFixed(2);
-		data.value.shiZhi1000.percent = (data.value.shiZhi1000.count / theCount * 100).toFixed(2);
-		data.value.shiZhi2000.percent = (data.value.shiZhi2000.count / theCount * 100).toFixed(2);
-		data.value.shiZhi5000.percent = (data.value.shiZhi5000.count / theCount * 100).toFixed(2);
-		data.value.shiZhi10000.percent = (data.value.shiZhi10000.count / theCount * 100).toFixed(2);
+		data.value.shiZhi1.percent = (data.value.shiZhi1.count / theCount * 100).toFixed(2);
+		data.value.shiZhi2.percent = (data.value.shiZhi2.count / theCount * 100).toFixed(2);
+		data.value.shiZhi3.percent = (data.value.shiZhi3.count / theCount * 100).toFixed(2);
+		data.value.shiZhi4.percent = (data.value.shiZhi4.count / theCount * 100).toFixed(2);
+		data.value.shiZhi5.percent = (data.value.shiZhi5.count / theCount * 100).toFixed(2);
+		data.value.shiZhi6.percent = (data.value.shiZhi6.count / theCount * 100).toFixed(2);
 	}
 	data.value.shiZhiList = [
 		{
 			shiZhi0: data.value.shiZhi0,
-			shiZhi100: data.value.shiZhi100,
-			shiZhi500: data.value.shiZhi500,
-			shiZhi1000: data.value.shiZhi1000,
-			shiZhi2000: data.value.shiZhi2000,
-			shiZhi5000: data.value.shiZhi5000,
-			shiZhi10000: data.value.shiZhi10000
+			shiZhi1: data.value.shiZhi1,
+			shiZhi2: data.value.shiZhi2,
+			shiZhi3: data.value.shiZhi3,
+			shiZhi4: data.value.shiZhi4,
+			shiZhi5: data.value.shiZhi5,
+			shiZhi6: data.value.shiZhi6
 		}
 	];
 	store.updateStockMarketStats({
@@ -308,6 +307,7 @@ async function requestAllStockDetail() {
 		shiZhiList: data.value.shiZhiList,
 		updatedAt: new Date().toISOString()
 	});
+	console.log('requestAllStockDetail done');
 }
 
 async function requestStockDetail(stock) {
@@ -331,13 +331,14 @@ async function requestAllDailyBasic() {
 	let allStocks = store.allStocks || [];
 	let concurrence = 200;
 	let compositeIndex = {
+		index: {}, // 整个大盘的数据
 		index0: {},
-		index100: {},
-		index500: {},
-		index1000: {},
-		index2000: {},
-		index5000: {},
-		index10000: {},
+		index1: {},
+		index2: {},
+		index3: {},
+		index4: {},
+		index5: {},
+		index6: {},
 	};
 	for (let i = 0; i < allStocks.length; i += concurrence) {
 		console.log('requestAllDailyBasic', i, new Date().toISOString());
@@ -360,25 +361,31 @@ async function requestAllDailyBasic() {
 			if (newTotalMV < 100) {
 				indexNum = 'index0';
 			} else if (newTotalMV < 500) {
-				indexNum = 'index100';
+				indexNum = 'index1';
 			} else if (newTotalMV < 1000) {
-				indexNum = 'index500';
+				indexNum = 'index2';
 			} else if (newTotalMV < 2000) {
-				indexNum = 'index1000';
+				indexNum = 'index3';
 			} else if (newTotalMV < 5000) {
-				indexNum = 'index2000';
+				indexNum = 'index4';
 			} else if (newTotalMV < 10000) {
-				indexNum = 'index5000';
+				indexNum = 'index5';
 			} else {
-				indexNum = 'index10000';
+				indexNum = 'index6';
 			}
 			stock.items.forEach(item => {
 				compositeIndex[indexNum][item.trade_date] = compositeIndex[indexNum][item.trade_date] || {
 					amount: 0,
 					count: 0
 				};
+				compositeIndex['index'][item.trade_date] = compositeIndex['index'][item.trade_date] || {
+					amount: 0,
+					count: 0
+				};
 				compositeIndex[indexNum][item.trade_date].count += 1;
 				compositeIndex[indexNum][item.trade_date].amount += item.total_mv;
+				compositeIndex['index'][item.trade_date].count += 1;
+				compositeIndex['index'][item.trade_date].amount += item.total_mv;
 			});
 		}
 		let endTime = new Date().getTime();
@@ -394,6 +401,7 @@ async function requestAllDailyBasic() {
 		...compositeIndex,
 		updatedAt: new Date().toISOString()
 	});
+	console.log('requestAllDailyBasic done');
 }
 
 async function requestDailyBasic(stock) {
@@ -424,7 +432,7 @@ async function requestDailyBasic(stock) {
 	let items = res.data.data.items.map((item) => {
 		return {
 			trade_date: item[1], // 交易日期
-			total_mv: item[16], // / 10000, // 总市值原始数据是万元，转换为亿
+			total_mv: item[16] / 10000, // 总市值原始数据是万元，转换为亿
 		}
 	});
 	return {
