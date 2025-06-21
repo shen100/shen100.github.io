efe48f0248ee82f851890929e48dca70701e8f62
刘珅 <liushen@habby.com>
2025-06-22 01:08:27 +0800

    update

--- src/modules/docs/views/All.vue
+++ src/modules/docs/views/All.vue
@@ -4,7 +4,7 @@
         <p>This section contains all the documentation available for the project.</p>
         <ul>
             <li><a href="/docs/1/chapter/1">Godot相关文档</a></li>
-            <li><a href="/docs/1/chapter/2">开发辅助工具</a></li>
+            <li><a href="/docs/2/chapter/1">开发辅助工具</a></li>
         </ul>
     </div>
 </template>
