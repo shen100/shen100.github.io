21f16a2d396eb8f566584280218ad63feb178ee6
liushen <liushen_shen@163.com>
2026-06-21 20:28:23 +0800

    update

--- src/modules/trade/util/stock.js
+++ src/modules/trade/util/stock.js
@@ -15,3 +15,27 @@ export async function requestStockDetail(stock) {
 		price: Number(arr[3] || '3'), // 当前价格  
 	}
 }
+
+export async function requestMonthK(stock, start, end, count) {
+	let url = "https://web.ifzq.gtimg.cn/appstock/app/fqkline/get?param=";
+	url += (stock.stockFullId + ",month," + start + "," + end + "," + count + ",qfq");
+
+	let res = await axios.get(url);
+	
+	if (!(res.data && res.data.data)) {
+		return null;
+    }
+
+	let myKList = [];
+    if (res.data.data[stock.stockFullId]['qfqmonth']) {
+		myKList = res.data.data[stock.stockFullId].qfqmonth;
+    } else {
+		myKList = res.data.data[stock.stockFullId].month;
+    }
+		
+	let dates = []
+	for (let i = 0; i < myKList.length; i++) {
+		dates.push(myKList[i][0]); // 之前请求成交量用了dates
+    }
+	return myKList;
+}
